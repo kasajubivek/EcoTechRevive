83ee469f417833eef3b33ea70b1e8dc5c48a32b4
dsalgos <ds.algos143@gmail.com>
2024-06-24 19:36:23 -0400

    Adding comments for Views(Controllers) and Overridden class 'NoCacheMiddleware'

--- main/middleware.py
+++ main/middleware.py
@@ -1,6 +1,12 @@
 from django.utils.deprecation import MiddlewareMixin
 
 
+# overriding the default middleware implementation
+# to make sure pages are not cached.
+# This is a less data-centric application and removing
+# cache and revalidating the user, prevents unauthorized
+# access to teh webpages by not allowing access via 'back'
+# 'forward' button on browser.
 class NoCacheMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
